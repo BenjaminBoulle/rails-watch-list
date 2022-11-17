e43c28797ba4b5c1b9e35c648c088ee717e387b7
Benjamin Boulle <benjaminboulle.sjb@gmail.com>
2022-11-17 16:38:13 +0100

    on a fini euh

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6 }
-  # validates  ,uniqueness: true
-  validates_uniqueness_of :list_id, :scope => [:movie_id]
+  validates_uniqueness_of :list_id, scope: :movie_id
 end
